00cdf6bda12b07ebd118b8f9195737b796c17c78
MaidScientistIzutsumiMarin <>
2025-10-26 02:56:07 -0400

    Improve build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,14 @@
-Remove-Item build, dist -Recurse
-nicegui-pack.exe --name "psvr2toolkit-installer" --windowed --onefile src\psvr2toolkit_installer\__main__.py
-Move-Item dist\psvr2toolkit-installer.exe .
-Remove-Item build, dist -Recurse
-nicegui-pack.exe --name "psvr2toolkit-installer" --windowed src\psvr2toolkit_installer\__main__.py
+function Invoke-NiceGUIPack {
+    param ([switch]$OneFile)
+    Remove-Item build, dist -Recurse -ErrorAction SilentlyContinue
+    nicegui-pack.exe --name psvr2toolkit-installer --windowed ($OneFile ? '--onefile' : '--') src\psvr2toolkit_installer\__main__.py
+}
+
+Invoke-NiceGUIPack
+7z.exe a dist\psvr2toolkit-installer
+Move-Item dist\psvr2toolkit-installer.7z .
+Invoke-NiceGUIPack -OneFile
+Move-Item dist\psvr2toolkit-installer\* .
+#flit.exe publish
+Remove-Item build, dist -Recurse -ErrorAction SilentlyContinue
 Pause
