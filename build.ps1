0474a20ff732163a15062363efdb39eb94467500
MaidScientistIzutsumiMarin <>
2025-11-09 19:03:07 -0500

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-..\..\Powershell\build.ps1 'githubkit/rest/__init__.py', 'githubkit/versions/v2022_11_28/models/__init__.py' -ExtraArgs '--collect-data nicegui --collect-data mscerts --optimize 1'
+..\..\Powershell\build.ps1 'githubkit/rest/__init__.py', 'githubkit/versions/v2022_11_28/models/__init__.py' -ExtraArgs '--collect-data mscerts --collect-all nicegui --optimize 1'
